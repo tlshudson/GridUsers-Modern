80ead3ef4fc3aa2cc3dffa09630c9e5bdbcff107
Hudson Teles <hudsonteles00@email.com>
2026-02-19 16:45:41 -0300

    feat: create api users, controller User

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(8)->create([
+            // 'name' => 'Test User',
+            // 'email' => 'test@example.com',
+            'data_nascimento' => Carbon::now()->subYear(20)->format('Y-m-d')
+        ]);
     }
 }
